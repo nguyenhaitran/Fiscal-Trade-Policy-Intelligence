1cac6928a924aed4f629950aef2d9ec86fbb12dc
henrytran <126839412+nguyenhaitran@users.noreply.github.com>
2026-04-01 00:33:34 +0800

    fixed the fetch data to fetch the data into 3 smaller worksheets and 1 big master for client

--- fetch_data/fetch_data.py
+++ fetch_data/fetch_data.py
@@ -9,12 +9,16 @@ from oauth2client.service_account import ServiceAccountCredentials
 # %%
 # CONFIGURATION
 # Include required fields
-FIELDS = [
-    'cusip', 'issueDate', 'securityType', 'securityTerm', 'auctionDateYear', 'maturityDate', 'datedDate', 'maturingDate',
-    'auctionFormat', 'closingTimeCompetitive', 'offeringAmount', 'allocationPercentage', 'totalTendered', 'totalAccepted', 'bidToCoverRatio',
-    'interestRate', 'highYield', 'lowYield', 'averageMedianYield', 'highDiscountRate', 'lowDiscountRate', 'highInvestmentRate', 'lowInvestmentRate',
-    'highPrice', 'lowPrice', 'pricePer100', 'updatedTimestamp'
+security_fields = ['cusip', 'securityType', 'securityTerm', 'originalSecurityTerm', 'series', 'corpusCusip', 'interestRate', 'tips', 'floatingRate', 'callable', 'callDate']
+
+auction_fields = ['cusip', 'auctionDate', 'auctionDateYear', 'announcementDate', 'issueDate', 'maturityDate', 'datedDate', 'maturingDate', 'auctionFormat','closingTimeCompetitive',
+            'closingTimeNoncompetitive', 'offeringAmount', 'allocationPercentage', 'totalTendered', 'totalAccepted', 'bidToCoverRatio', 'interestRate', 'highYield', 'lowYield',
+            'averageMedianYield', 'highDiscountRate', 'lowDiscountRate', 'highInvestmentRate', 'lowInvestmentRate', 'highPrice', 'lowPrice', 'pricePer100', 'updatedTimestamp'
 ]
+
+bidder_fields = ['cusip', 'totalAccepted', 'primaryDealerAccepted', 'directBidderAccepted', 'indirectBidderAccepted', 'fimaNoncompetitiveAccepted', 'somaAccepted', 
+                'competitiveAccepted', 'noncompetitiveAccepted','treasuryRetailAccepted']
+
 
 # Excel file to keep updating
 base_path = os.path.dirname(os.path.abspath(__file__)) # get the original place of the script
@@ -29,56 +33,54 @@ def get_data():
     response = requests.get(API_URL)
     new_records = response.json()
 
-    # SELECTED REQUIRED FIELDS ONLY
+    # Only keep the required fields
     df = pd.DataFrame(new_records['securityList'])
+
     # check if required field existed in the API response
-    required_fields = [field for field in FIELDS if field in df.columns]
-    new_df = df[required_fields].copy()
+    #security_required_fields = [field for field in security_fields if field in df.columns]
+    #auction_required_fields = [field for field in auction_fields if field in df.columns]
+    #bidder_required_fields = [field for field in bidder_fields if field in df.columns]
+    security_new_df = df[security_fields].copy()
+    auction_new_df = df[auction_fields].copy()
+    bidder_new_df = df[bidder_fields].copy()
+
+    # full worksheet copy
+    master_df = df.copy()
+    #master_df['issueDate'] = master_df['issueDate'].dt.strftime('%Y-%m-%d')
+    master_df = master_df.fillna('')
 
     # Put the data into the speadsheet
     # Step 1: Setup the connection
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/henrytran/Documents/GitHub/Fiscal-Trade-Policy-Intelligence/Robot Key/Fiscal Index Intelligence.json', scope) # take the credential
     client = gspread.authorize(creds)
+    file = client.open('Fiscal_Trade_Index')
 
-    # Step 2: Open the Sheet
-    sheet = client.open("Fiscal_Trade_Index").worksheet("US_Auction")
+    # Step 2: Open the sheets
+    worksheets = {
+        "Security": security_new_df,
+        "Auction": auction_new_df,
+        "Bidder": bidder_new_df,
+        "Master": master_df  # The merged version
+    }
 
-    # HANDLE NEW RECORDS AND ADD TO EXISTING EXCEL FILE (ONLY IF AVAILABLE)
-    if os.path.exists(CSV_FILE):
-        existing_df = pd.read_csv(CSV_FILE)
-        # using CUSIP as unique ID, ensuring no same auction added
-        new_df['unique_id'] = new_df['cusip'].astype('str')
-        existing_df['unique_id'] = existing_df['cusip'].astype('str')
-        # filter the data, only keep new/non-existing records
-        records_to_add = new_df[~new_df['unique_id'].isin(existing_df['unique_id'])].copy()
-        # remove the helper column
-        records_to_add = records_to_add.drop(columns=['unique_id'])
+    # Step 3: Loading the data into the correct table
+    for worksheet, df in worksheets.items():
+        try:
+            sheet = file.worksheet(worksheet)
+        except:
+            sheet = file.add_worksheet(title=worksheet, rows="100", cols="20")
         
-        # add new data to the current csv file
-        if not records_to_add.empty:
-            final_df = pd.concat([records_to_add, existing_df], ignore_index=True) # old one first, new one after
-            
-            # Step 3: Prepare the data
-            # Use .fillna('') to ensure no 'NaN' values in case of empty values
-            data_to_upload = [final_df.columns.values.tolist()] + final_df.fillna('').values.tolist()
+        # data to upload
+        upload_data = [df.columns.values.tolist()] + df.fillna('').values.tolist()
 
-            final_df.to_csv(CSV_FILE, index=False)
-
-            # Step 4: Upload to Google Cloud
-            sheet.clear() # Clears old data so you don't have duplicates
-            sheet.update('A1', data_to_upload)
-
-            print("Data successfully synced to Google Sheets!")
-
-            print('There are {} new records!'.format(len(records_to_add)))
-        else:
-            print('No new records found!')
-    else:
-        # create new csv file if not existed
-        new_df.to_csv(CSV_FILE, index=False)
-        print('New CSV file created!')
+        sheet.clear()
+        sheet.update('A1', upload_data)
+        print(f"Successfully updated {worksheet}")
+    
+    # Keep one CSV copy
+    master_df.to_csv(CSV_FILE, index=False)
+    print(f"Backup saved to {CSV_FILE}")
 
 if __name__ == "__main__":
     get_data()
-
